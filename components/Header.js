81bc6392c8b72f2eae9b01ebf76541d1b3d35e73
DevWithEasy <devwitheasy@gmail.com>
2022-12-01 16:42:47 +0600

    heade add

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,17 @@
+import { useState } from "react"
+
 export default function Header(){
+    const [menu,setMenu] = useState(false)
     return(
+        
         <div className="header">
             <h1>কুইজ  বক্স</h1>
             <div className="">
-                <button>+</button>
-                <div className="">
+                <button onClick={()=>setMenu(!menu)}>+</button>
+                {menu && <div className="">
                     <button>Profile</button>
                     <button>logout</button>
-                </div>
+                </div>}
             </div>
         </div>
     )
