6ffee6f311c9f3434c16fc503ddc21a008327c84
DevWithEasy <devwitheasy@gmail.com>
2022-12-05 08:38:09 +0600

    single quiz page error fix

--- components/Start.js
+++ components/Start.js
@@ -8,8 +8,8 @@ export default function Start({setStart}){
         <div className="start">
           <div className="">
                 <p>আপনি কুইজের বিষয় পছন্দ করেছেন:</p>
-                <p className="subject">{category.category}</p>
-                <p>এই বিভাগে মোট কুইজ আছে {category.totalData} টি।</p>
+                <p className="subject">{category?.category}</p>
+                <p>এই বিভাগে মোট কুইজ আছে {category?.totalData} টি।</p>
                 <button onClick={() => startQuiz(setStart)}>শুরু করুন</button>
           </div>
         </div>
